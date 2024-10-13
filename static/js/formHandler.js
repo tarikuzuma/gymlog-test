dbe1e724ae29f833f39374c0d759a97f46cda0b9
Tarikuzuma <edwinjrgumba@gmail.com>
2024-10-14 02:33:26 +0800

    refactor: made it so that hidden history is in js

--- static/js/formHandler.js
+++ static/js/formHandler.js
@@ -20,3 +20,10 @@ document.addEventListener('DOMContentLoaded', function() {
     autoSubmitForm();
     autoFocusInput();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => {
+        form.setAttribute('autocomplete', 'off');
+    });
+});
