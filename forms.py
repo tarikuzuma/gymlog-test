81a0f1fbe6c53c1854fc07d1928d84d6a20b4ea2
Tarikuzuma <edwinjrgumba@gmail.com>
2024-11-09 01:40:28 +0800

    chore: modified forms.py

--- forms.py
+++ forms.py
@@ -9,11 +9,11 @@ class RegGymLogForm(Form):
     full_name = StringField('Full Name', [validators.Length(min=2, max=100), validators.DataRequired()])
     student_id = StringField('Student ID Number', [validators.Length(min=9, max=13), validators.DataRequired()])
     pe_course = wtforms.SelectField('PE Course', choices=[
-        ('pedu1', 'PEEDU1'),
-        ('pedu2', 'PEEDU2'),
-        ('pedu3', 'PEEDU3'),
-        ('pedu4', 'PEEDU4'),
-        ('none', 'None')
+        ('PE1', 'PEEDU1'),
+        ('PE2', 'PEEDU2'),
+        ('PE3', 'PEEDU3'),
+        ('PE4', 'PEEDU4'),
+        ('None', 'None')
     ], validators=[validators.DataRequired()])
     enrolled_block = StringField('Enrolled Block', [validators.Length(min=3, max=9), validators.DataRequired()])
     rfid = StringField('APC Identification Card', [validators.DataRequired()])
